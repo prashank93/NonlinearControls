11410d887fde682bd069246121cc8813601a4a09
Noah Fox <fox.noah@gmail.com>
2015-05-08 14:42:44 -0400

    sontag switching "works"

---
+++ Sontag/student_controller.m
@@ -0,0 +1,40 @@
+% Student supplied function to compute the control input at each instant of time
+% Input parameters:
+%   t  -  Time (in seconds) since start of simulation
+%   x - state of the rocket, x=[y,z,th,psi,dy,dz,dth,dpsi,m]^T
+%   consts - structure that contains various system constants
+%   ctrl  -  any student defined control parameters
+% Output parameters:
+%   u  -  [thrust; torque] - two inputs to the rocket
+function u = student_controller(t, x, consts, ctrl)
+global done
+
+% LfV = eval(subs(ctrl.LfV,sym_vec,x));
+% LgV = eval(subs(ctrl.LgV,sym_vec,x));
+
+if done == 0
+    x(2) = x(2) - 150;
+end
+
+LfV = ctrl.LfVfun(x(1),x(2),x(3),x(4),x(5),x(6),x(7),x(8),x(9));
+LgV = ctrl.LgVfun(x(1),x(2),x(3),x(4),x(5),x(6),x(7),x(8),x(9));
+% a = LfV;
+% b = LgV*LgV';
+% if LgV ~= 0
+%     u = -((a + sqrt(a^2 + b^4))/b)*LgV';
+% else
+%     u = [0;0];
+% end
+
+if all(LgV ~= 0)
+    u(1,1) = -((LfV + sqrt(LfV^2 + LgV(1)^4))/LgV(1));
+    u(2,1) = -((LfV + sqrt(LfV^2 + LgV(2)^4))/LgV(2));
+else
+    u = [0;0];
+end
+
+if abs(x(3)) < 0.05 && abs(x(7)) < .1 && abs(x(5)) < 5 || done == 1
+    u = -ctrl.K*x;
+    done = 1;
+end
+end
---
+++ Sontag/student_setup.m
@@ -0,0 +1,91 @@
+% Function to setup and perform any one-time computations
+% Input parameters
+%   x - state of the rocket, x=[y,z,th,psi,dy,dz,dth,dpsi,m]^T
+%   consts - structure that contains various system constants
+% Output parameters
+%   ctrl  -  any student defined control parameters
+function ctrl = student_setup(x0, consts)
+global done
+done = 0;
+qr = [0.001 0.001 10 0.001 0.001 0.001 10 0.001 0.001 1 1];
+[~,P] = lqrmaker(qr);
+K = [ -0.7544 1.1544 0 0 -5.5728 8.5728 0 0 -0.1575;
+          0.1306    0  -37.6338   72.8933    1.6009    0  -48.6607   30.3236    0];
+% [K] = lqrmaker(qr2);
+syms x1 x2 x3 x4 x5 x6 x7 x8 x9 real
+
+f_vec = [   x5
+    x6
+    x7
+    x8
+    0
+    -consts.g
+    0
+    0
+    0] ;
+
+g_vec = [0,    0 ;
+    0,    0 ;
+    0,    0 ;
+    0,    0 ;
+    -consts.gamma*sin(x4+x3)/x9,    0 ;
+    consts.gamma*cos(x4+x3)/x9,    0 ;
+    -consts.L*consts.gamma*sin(x4)/consts.J,    0 ;
+    0, 1/consts.JT ;
+    -1,    0] ;
+
+x = [x1 x2 x3 x4 x5 x6 x7 x8 x9]';
+V = x'*P*x;
+LfV(x1,x2,x3,x4,x5,x6,x7,x8,x9) = jacobian(V,x)*f_vec;
+LgV(x1,x2,x3,x4,x5,x6,x7,x8,x9) = jacobian(V,x)*g_vec;
+ctrl.LfVfun = matlabFunction(LfV);
+ctrl.LgVfun = matlabFunction(LgV);
+ctrl.K = K;
+end
+
+function [K,P] = lqrmaker(qr)
+consts = get_consts();
+global target
+syms y z th ps dy dz dth dpsi m ft T real
+
+f_vec = [   dy
+            dz
+            dth
+            dpsi
+            0
+            -consts.g
+            0
+            0
+            0] ;
+
+g_vec = [0,    0 ;
+        0,    0 ;
+        0,    0 ;
+        0,    0 ;
+        -consts.gamma*sin(ps+th)/m,    0 ;
+        consts.gamma*cos(ps+th)/m,    0 ;
+        -consts.L*consts.gamma*sin(ps)/consts.J,    0 ;
+        0, 1/consts.JT ;
+        -1,    0] ;
+
+x = [y z th ps dy dz dth dpsi m];
+u = [ft; T];
+
+sys = f_vec + g_vec*u;
+
+ft_0 = (consts.m_nofuel*consts.g)/consts.gamma;
+
+target = [0 consts.L 0 0 0 0 0 0 consts.max.m_fuel];
+eq = [0 consts.L 0 0 0 0 0 0 consts.max.m_fuel 1.4715 0];
+eq_vars = [y z th ps dy dz dth dpsi m ft T];
+
+A_sym = jacobian(sys,x);
+B_sym = jacobian(sys,u);
+
+A = eval(subs(A_sym,eq_vars,eq));
+B = eval(subs(B_sym,eq_vars,eq));
+
+Q = diag(abs(qr(1:9)));
+R = diag(abs(qr(10:11)));
+[K,P] = lqr(A,B,Q,R);
+end
